6b4e50502909438fb523db9ea8080c7482f25728
Tina Thomas <tinathomasedu@gmail.com>
2019-05-14 22:46:54 -0400

    pushing up a fun little exercise ... exercise

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,11 +8,64 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("HelloWorld");
-            Console.Write("Pause to read so press any key to contine...");
-            Console.ReadKey(true);
+            double runningTotal = 0;
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                // Prompt the user for minutes exercised 
+                Console.Write("Enter how many minues you exercised or type \"quit\" to exit: ");
+                string entry = Console.ReadLine();
+
+                if (entry.ToLower() == "quit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    try
+                    {
+                        // Add minutes exercised to total 
+                        var minutes = double.Parse(entry);
+
+                        if(minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                            continue;
+                        }
+                        else if(minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing, am I right?");
+                        }
+                        else if(minutes <= 30)
+                        {
+                            Console.WriteLine("Way to go hot stuff!");
+                        }
+                        else if(minutes <= 60)
+                        {
+                            Console.WriteLine("You must be a ninja warrior in training!");
+                        }
+                        else
+                        {        
+                            Console.WriteLine("Okay, now you're just showing off!");
+                        }
+
+                        runningTotal += minutes;
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("That is not valid input.");
+                        continue;
+                    }
+                    
+                }
+                // repeat until the user quits 
+
+            }
+
+            Console.WriteLine("Goodbye");
         }
     }
 }
