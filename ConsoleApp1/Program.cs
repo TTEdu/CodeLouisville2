bd8bdf1a23b77e156cdef12464c0ec3e225b50de
Tina Thomas <tinathomasedu@gmail.com>
2019-05-15 00:36:47 -0400

    refractor code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,10 +10,9 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            double runningTotal = 0;
-            bool keepGoing = true;
+            var runningTotal = 0.0;
 
-            while (keepGoing)
+            while (true)
             {
                 // Prompt the user for minutes exercised 
                 Console.Write("Enter how many minues you exercised or type \"quit\" to exit: ");
@@ -21,50 +20,46 @@ namespace ConsoleApp1
 
                 if (entry.ToLower() == "quit")
                 {
-                    keepGoing = false;
+                    break;
                 }
-                else
+                
+                try
                 {
-                    try
-                    {
-                        // Add minutes exercised to total 
-                        var minutes = double.Parse(entry);
+                    // Add minutes exercised to total 
+                    var minutes = double.Parse(entry);
 
-                        if(minutes <= 0)
-                        {
-                            Console.WriteLine(minutes + " is not an acceptable value.");
-                            continue;
-                        }
-                        else if(minutes <= 10)
-                        {
-                            Console.WriteLine("Better than nothing, am I right?");
-                        }
-                        else if(minutes <= 30)
-                        {
-                            Console.WriteLine("Way to go hot stuff!");
-                        }
-                        else if(minutes <= 60)
-                        {
-                            Console.WriteLine("You must be a ninja warrior in training!");
-                        }
-                        else
-                        {        
-                            Console.WriteLine("Okay, now you're just showing off!");
-                        }
-
-                        runningTotal += minutes;
-                    }
-                    catch(FormatException)
+                    if (minutes <= 0)
                     {
-                        Console.WriteLine("That is not valid input.");
+                        Console.WriteLine(minutes + " is not an acceptable value.");
                         continue;
                     }
-                    
+                    else if (minutes <= 10)
+                    {
+                        Console.WriteLine("Better than nothing, am I right?");
+                    }
+                    else if (minutes <= 30)
+                    {
+                        Console.WriteLine("Way to go hot stuff!");
+                    }
+                    else if (minutes <= 60)
+                    {
+                        Console.WriteLine("You must be a ninja warrior in training!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Okay, now you're just showing off!");
+                    }
+
+                    runningTotal += minutes;
                 }
-                // repeat until the user quits 
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not valid input.");
+                    continue;
+                }
 
             }
-
+                // repeat until the user quits 
             Console.WriteLine("Goodbye");
         }
     }
